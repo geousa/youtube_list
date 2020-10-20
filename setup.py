4cf8dc7c3214735e69b604b1f3c3c37f2cd25709
geousa <73154694+geousa@users.noreply.github.com>
2020-10-20 12:12:48 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='yt_search',
     version='0.1.2',
     packages=['yt_list'],
-    url='https://github.com/youtube_list/yt_search',
+    url='https://github.com/geousa/youtube_list',
     license='GPL-3.0 License',
     author='GEO',
     install_requires=["bs4", "requests", "requests_cache"],
