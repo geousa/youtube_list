13f46572eea7ee5fb09afa223fbac2e38076241d
geousa <73154694+geousa@users.noreply.github.com>
2020-10-20 12:10:43 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='youtube_searcher',
+    name='yt_search',
     version='0.1.2',
     packages=['yt_list'],
     url='https://github.com/youtube_list/yt_search',
@@ -9,5 +9,5 @@ setup(
     author='GEO',
     install_requires=["bs4", "requests", "requests_cache"],
     author_email='geocipto@gmail.com',
-    description='youtube list'
+    description='youtube'
 )
